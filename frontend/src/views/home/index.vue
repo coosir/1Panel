7c34dca52eeddd812a1c840bb5a7e6d490170b80
coosir <git@coosir.com>
2025-12-29 13:21:28 +0800

    版本展示优化

--- frontend/src/views/home/index.vue
+++ frontend/src/views/home/index.vue
@@ -169,7 +169,7 @@
             <el-col :xs="24" :sm="24" :md="8" :lg="8" :xl="8">
                 <CardWithHeader :header="$t('home.systemInfo')">
                     <template #body>
-                        <div class="h-systemInfo" style="height:auto;">
+                        <div class="h-systemInfo" style="height: auto">
                             <el-descriptions :column="1" border>
                                 <el-descriptions-item class-name="system-content" label-class-name="system-label">
                                     <template #label>
@@ -249,7 +249,7 @@
                                     <template #label>
                                         <span>面板版本</span>
                                     </template>
-                                    1.10.33
+                                    1.10.33 (251229)
                                 </el-descriptions-item>
                             </el-descriptions>
                         </div>
